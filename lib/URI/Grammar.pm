ed931e969515c3256a1af498729356bb65af0762
U-ron-PC\ron <ron@ron-PC.(none)>
2009-05-11 06:25:04 -0400

    Modify grammar to use IETF grammars rather than IETF regex.

--- lib/URI/Grammar.pm
+++ lib/URI/Grammar.pm
@@ -1,25 +1,28 @@
 use v6;
-grammar URI::Grammar {
-    token TOP        { ^ [<scheme> ':']? [ '//' <authority>]? <path> ['?' <query>]? ['#' <fragment>]? $ };
-    token scheme     { <-[:/&?#]>+ };
-    token authority  { <host> [':' <port>]? };
-    token host       { <-[/&?#:]>* };
-    token port       { \d+ };
-    token path       { <slash>? [ <chunk> '/'?]* }; # * mb wrong, because that allow '' URI
-    token slash      { '/' };
-    token chunk      { <-[/?#:]>+ };
-    token query      { <-[#]>* };
-    token fragment   { .* };
-}
 
-# Official regexp (p5):
-# my($scheme, $authority, $path, $query, $fragment) =
-# $uri =~ m/
-#           (?:([^:/?#]+):)?
-#           (?://([^/?#]*))?
-#           ([^?#]*)
-#           (?:\?([^#]*))?
-#           (?:#(.*))?
-#         /x;
+use IETF::RFC_Grammar::URI;
+
+grammar URI::Grammar is IETF::RFC_Grammar::URI {
+    
+    token TOP               {
+        ^   [<scheme> ':']?
+            [ '//' <authority>]? <path> ['?' <query>]?
+            [ '#' <fragment> ]? $
+    };
+
+    token authority  { <host> [':' <port>]? };
+    token path       { <slash>? [ <chunk>** '/'?]* };
+    token slash      { '/' };
+
+#
+# following hangs rakudo (RT #37745 afaik)
+# token chunk { <[a..z]>* }; say 'ok' if 'index/' ~~ /[ <chunk> '/'?]*/
+#
+# so can't just use rfc segment but use small hack of requiring
+# at least one char.  Use of ** seperator above makes behavior
+# consistent with rfc.
+#
+    token chunk      { <.pchar> <.segment> }
+}
 
 # vim:ft=perl6
